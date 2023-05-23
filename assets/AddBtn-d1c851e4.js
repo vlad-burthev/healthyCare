1725a290197a38d6ca521b37fbd19837adeaffa9
Vlad <burthev04@gmail.com>
2023-05-23 19:00:49 +0300

    Updates

---
+++ assets/AddBtn-d1c851e4.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-fc83b678.js";const s={"btn-add":"_btn-add_5tqxw_1"},a=t=>n.jsx("button",{...t,className:`${s["btn-add"]}  ${t.className}`,children:t.children});export{a as A};
--- assets/AddBtn-d6c8b551.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-8caec843.js";const s={"btn-add":"_btn-add_5tqxw_1"},a=t=>n.jsx("button",{...t,className:`${s["btn-add"]}  ${t.className}`,children:t.children});export{a as A};
